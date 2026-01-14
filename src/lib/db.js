60322215ec5189cced1000281868182555dba9b1
Keshav <keshavkhandelwal30@gmail.com>
2026-01-14 13:01:37 +0530

    First Commit

---
+++ src/lib/db.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const MONGODB_URL = process.env.MONGO_URI;
+
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = {
+        conn: null,
+        promise: null,
+    };
+}
+
+export const connect = async () => {
+    if (cached.conn) return cached.conn;
+
+    cached.promise =
+        cached.promise ||
+        mongoose.connect(MONGODB_URL, {
+            dbName: "BlogDB",
+            bufferCommands: false,
+            connectTimeoutMS: 30000,
+        });
+
+    cached.conn = await cached.promise;
+
+    return cached.conn;
+};
